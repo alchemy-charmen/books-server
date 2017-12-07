5e6931eef7f5510f657be7fedbb4bb33210c4503
CharlyWelch <blwbiology@gmail.com>
2017-12-07 15:16:08 -0800

    add loadBooks function to build database table

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require ('dotenv').config();
 const express = require('express');
 const app = express();
 const pg = require('pg');
+const fs = require('fs');
 const PORT = process.env.PORT;
 
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -18,6 +19,24 @@ app.get('/api/v1/books', (req, res) => {
         .then(data => res.send(data.rows));
 });
 
+
+// query to endpoint that will retrieve a single book based on an id
+// app.get('/api/v1/books/:id', (req, res) => {
+//     client.query(`;`)
+//         .then(data => res.send(data.rows));
+// });
 // app.get('/api/v1/about', (req, res) => {
 //     client.query()
 // });
+
+function loadBooks() {
+    fs.readFile('/books.json', function(err, fd) {
+        JSON.parse(fd.toString()).forEach(function(ele) {
+            client.query(
+                'INSERT INTO books (author, title, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
+                [ele.author, ele.title, ele.isbn, ele.image_url, ele.description]
+            );
+        });
+    });
+}
+loadBooks();
