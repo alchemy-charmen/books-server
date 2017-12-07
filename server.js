2c059ef76208cbb2c1a307f7ed80776803732102
CharlyWelch <blwbiology@gmail.com>
2017-12-07 15:36:30 -0800

    load database with json file!!

--- server.js
+++ server.js
@@ -10,14 +10,17 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 client.connect();
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
-
 app.get('/api/v1/books', (req, res) => {
     client.query(`SELECT * FROM books;`)
         .then(data => res.send(data.rows));
 });
+
+loadDB();
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
 
 
 // query to endpoint that will retrieve a single book based on an id
@@ -30,7 +33,7 @@ app.get('/api/v1/books', (req, res) => {
 // });
 
 function loadBooks() {
-    fs.readFile('/books.json', function(err, fd) {
+    fs.readFile('./books.json', function(err, fd) {
         JSON.parse(fd.toString()).forEach(function(ele) {
             client.query(
                 'INSERT INTO books (author, title, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
@@ -39,4 +42,23 @@ function loadBooks() {
         });
     });
 }
-loadBooks();
+
+function loadDB() {
+    client.query(`
+    CREATE TABLE IF NOT EXISTS
+    books (
+      id SERIAL PRIMARY KEY,
+      author VARCHAR(255) UNIQUE NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      isbn VARCHAR(300) NOT NULL,
+      image_url VARCHAR(300) NOT NULL,
+      description TEXT NOT NULL
+    );`
+    )
+        .then(data => {
+            loadBooks(data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
